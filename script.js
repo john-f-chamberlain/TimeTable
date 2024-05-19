d4752c3872541b73ba302919105e40a7a2d82709
John F. Chamberlain <john.f.chamberlain@gmail.com>
2024-05-18 20:22:07 -0400

    Sat05-18-2024@20-22-07.14

--- script.js
+++ script.js
@@ -175,7 +175,7 @@ $(function () {
         let second = d.getSeconds();
 
         let dayOffset = event_days.indexOf(day);
-        let leftPx = (dayOffset * $px_day) + (hour + (minute / 60) + (second / 3600 ) * 240)
+        let leftPx = (dayOffset * $px_day) + ((hour + (minute / 60) + (second / 3600 )) * 240)
 
 
     }
